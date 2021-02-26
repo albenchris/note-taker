e8d5cfdfde2220703ec908fafde4720e39a12e54
albenchris <albenchris00@gmail.com>
2021-02-25 19:08:52 -0600

    added more middleware to parse data

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
-const fs = require("fs");
 const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const fs = require("fs");
 const path = require("path");
-const notes = fs.readFileSync("./data/notes.json");
+const uuid = require("uuid");
+let notes = fs.readFileSync("./data/notes.json", "utf-8");
 
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// function createNewNote(body, notesArr) {
+//     const note = body;
+//     notesArr.push(note);
+
+//     fs.writeFileSync(
+//         path.join(__dirname, "./data/notes.json"),
+//         JSON.stringify(notesArr, null, 2)
+//     );
+
+//     return note;
+// };
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/index.html"));
@@ -20,9 +35,12 @@ app.get("/api/notes", (req, res) => {
     res.json(JSON.parse(notes));
 });
 
-// app.get("/api/notes/:note", (req, res) => {
-
-// })
+// saves note into data/notes.json
+app.post("/api/notes", (req, res) => {
+    const newNote = req.body;
+    notes = JSON.parse(notes);
+    console.log(notes);
+});
 
 app.listen(PORT, () => {
     console.log(`Server now on port ${PORT}`);
